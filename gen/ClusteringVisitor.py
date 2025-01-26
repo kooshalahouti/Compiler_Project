1141bfc9dcdc76bff5d8760fc78b87ab98c763e9
TaraStrh <tara.setareh8282@gmail.com>
2025-01-26 14:03:26 -0800

    Fix:bug ast

--- gen/ClusteringVisitor.py
+++ gen/ClusteringVisitor.py
@@ -1,4 +1,4 @@
-# Generated from D:/uni/Compiler/finalProject/pythonProject/grammar/Clustering.g4 by ANTLR 4.13.1
+# Generated from F:/university/term 7/compiler/finalproject/Compiler_Project/grammar/Clustering.g4 by ANTLR 4.13.1
 from antlr4 import *
 if "." in __name__:
     from .ClusteringParser import ClusteringParser
@@ -8,11 +8,6 @@ else:
 # This class defines a complete generic visitor for a parse tree produced by ClusteringParser.
 
 class ClusteringVisitor(ParseTreeVisitor):
-
-    # Visit a parse tree produced by ClusteringParser#program.
-    def visitProgram(self, ctx:ClusteringParser.ProgramContext):
-        return self.visitChildren(ctx)
-
 
     # Visit a parse tree produced by ClusteringParser#cluster.
     def visitCluster(self, ctx:ClusteringParser.ClusterContext):
@@ -24,28 +19,43 @@ class ClusteringVisitor(ParseTreeVisitor):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by ClusteringParser#assign.
-    def visitAssign(self, ctx:ClusteringParser.AssignContext):
+    # Visit a parse tree produced by ClusteringParser#args.
+    def visitArgs(self, ctx:ClusteringParser.ArgsContext):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by ClusteringParser#expr.
-    def visitExpr(self, ctx:ClusteringParser.ExprContext):
+    # Visit a parse tree produced by ClusteringParser#n_clusters.
+    def visitN_clusters(self, ctx:ClusteringParser.N_clustersContext):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by ClusteringParser#plot.
-    def visitPlot(self, ctx:ClusteringParser.PlotContext):
+    # Visit a parse tree produced by ClusteringParser#n_iters.
+    def visitN_iters(self, ctx:ClusteringParser.N_itersContext):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by ClusteringParser#plot_args.
-    def visitPlot_args(self, ctx:ClusteringParser.Plot_argsContext):
+    # Visit a parse tree produced by ClusteringParser#random_state.
+    def visitRandom_state(self, ctx:ClusteringParser.Random_stateContext):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by ClusteringParser#dataset.
-    def visitDataset(self, ctx:ClusteringParser.DatasetContext):
+    # Visit a parse tree produced by ClusteringParser#epsilon.
+    def visitEpsilon(self, ctx:ClusteringParser.EpsilonContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by ClusteringParser#min_sample.
+    def visitMin_sample(self, ctx:ClusteringParser.Min_sampleContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by ClusteringParser#linkage.
+    def visitLinkage(self, ctx:ClusteringParser.LinkageContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by ClusteringParser#affinity.
+    def visitAffinity(self, ctx:ClusteringParser.AffinityContext):
         return self.visitChildren(ctx)
 
 
